2dbaf88c77a1a7c95588fbbfb3b32c80a7f5b945
un0211 <un0211@kaist.ac.kr>
2024-06-24 08:04:58 +0000

    ⚙️  Chore: tailwind 컬러 팔레트 및 반응형 브레이크 포인트 세팅 (#33)
    ⚙️  chore: tailwind 컬러 팔레트 및 반응형 브레이크 포인트 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return <main>main</main>;
-}
+const Home: React.FC = () => <div>Home</div>;
+
+export default Home;
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,9 @@
+import SignInForm from '@/containers/signin/SignInForm';
+
+const SignInPage: React.FC = () => (
+  <div>
+    <SignInForm />
+  </div>
+);
+
+export default SignInPage;
